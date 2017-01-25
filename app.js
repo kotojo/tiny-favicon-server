3d6303fdc8efb8c65bc1feac641d154ccfc1b2b5
Bruce Kellerman <brucek17@hotmail.com>
2017-01-25 20:23:38 +0000

    added favicon and routing for it

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
 const http = require('http')
+const fs = require('fs')
+const path = require('path')
 const request = require('request').defaults({ encoding: null, rejectUnauthorized: false })
 const faviconUrls = ['https://www.google.com/favicon.ico',
   'https://www.reddit.com/favicon.ico',
   'https://www.linkedin.com/favicon.ico']
 
 const server = http.createServer(function (request, response) {
-  let iconPromises = faviconUrls.map(iconUrl => {
-    return getIcon(iconUrl)
-  })
-  Promise.all(iconPromises).then(function (icons) {
-    response.writeHead(200, {'Content-Type': 'text/html'})
-    icons.map(icon => {
-      response.write(`<img src="data:image/x-icon;base64,${icon}" />`)
+  if (request.method === 'GET' && request.url === '/favicon.ico') {
+    response.writeHead(200, {'Content-Type': 'image/x-icon'})
+    fs.createReadStream(path.join(__dirname, 'favicon.ico')).pipe(response)
+  } else {
+    let iconPromises = faviconUrls.map(iconUrl => {
+      return getIcon(iconUrl)
     })
-    response.end()
-  }, function (errorMessage) {
-    response.writeHead(400)
-    response.end()
-    console.log(errorMessage)
-  })
+    Promise.all(iconPromises).then(function (icons) {
+      response.writeHead(200, {'Content-Type': 'text/html'})
+      icons.map(icon => {
+        response.write(`<img src="data:image/x-icon;base64,${icon}" />`)
+      })
+      response.end()
+    }, function (errorMessage) {
+      response.writeHead(400)
+      response.end()
+      console.log(errorMessage)
+    })
+  }
 })
 
 function getIcon (requestUrl) {
