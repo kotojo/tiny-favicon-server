4d018f4e901223284825f014cc68f75bfe777643
Bruce Kellerman <brucek17@hotmail.com>
2017-01-25 18:08:12 +0000

    inital code commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # tiny-favicon-server
+This is a little server for helping a friend figure out how to get favicons from a site and serve them all as images.
---
+++ app.js
@@ -0,0 +1,36 @@
+const http = require('http')
+const request = require('request').defaults({ encoding: null, rejectUnauthorized: false })
+const faviconUrls = ['https://www.google.com/favicon.ico',
+  'https://www.reddit.com/favicon.ico',
+  'https://www.linkedin.com/favicon.ico']
+
+const server = http.createServer(function (request, response) {
+  let iconPromises = faviconUrls.map(iconUrl => {
+    return getIcon(iconUrl)
+  })
+  Promise.all(iconPromises).then(function (icons) {
+    response.writeHead(200, {'Content-Type': 'text/html'})
+    icons.map(icon => {
+      response.write(`<img src="data:image/x-icon;base64,${icon}" />`)
+    })
+    response.end()
+  }, function (errorMessage) {
+    response.writeHead(400)
+    response.end()
+    console.log(errorMessage)
+  })
+})
+
+function getIcon (requestUrl) {
+  return new Promise(function (resolve, reject) {
+    request(requestUrl, function (error, response, body) {
+      if (error) {
+        reject(error)
+      }
+      const icon = Buffer.from(body).toString('base64')
+      resolve(icon)
+    })
+  })
+}
+
+server.listen(3000)
